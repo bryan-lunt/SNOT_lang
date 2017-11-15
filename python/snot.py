a29abb32621cf526810c281529fab4ece7f7d32d
Bryan Lunt <bjlunt2@illinois.edu>
2017-11-15 12:58:16 -0600

    made run on python3

--- python/snot.py
+++ python/snot.py
@@ -1,7 +1,22 @@
+from __future__ import unicode_literals
+
+import sys as _sys
+
 import numpy as _np
 import json as _J
 from collections import OrderedDict as _OD
-from itertools import count as _count,izip as _izip
+
+from itertools import count as _count
+
+#### VERSION DEPENDENT
+iteritems_attr = None
+if _sys.version_info[0] < 3:
+	from itertools import izip as _izip
+	iteritems_attr = "iteritems"
+else:
+	_izip = zip
+	from functools import reduce
+	iteritems_attr = "items"
 
 def load(filelike):
 	return _J.load(filelike,object_pairs_hook=_OD,parse_int=lambda x:float(x))
@@ -29,15 +44,15 @@ def traverse(in_snot):
 def _populate_py(in_snot,in_vector,begin=0):
 	num_consumed = 0
 	the_iter = None
-	if hasattr(in_snot,"values"):
-		the_iter = in_snot.iteritems();
+	if hasattr(in_snot,iteritems_attr):
+		the_iter = getattr(in_snot,iteritems_attr)();
 	elif isinstance(in_snot,list):
 		the_iter = _izip(_count(),in_snot)
 	else:
 		pass#should not descend into scalars.
 
 	for key,val in the_iter:
-		if hasattr(val, "values") or isinstance(val,list):
+		if hasattr(val, iteritems_attr) or isinstance(val,list):
 			num_consumed += _populate_py(val, in_vector,begin+num_consumed)
 		else:
 			in_snot[key] = in_vector[begin+num_consumed]
@@ -46,5 +61,17 @@ def _populate_py(in_snot,in_vector,begin=0):
 
 def populate(in_snot,in_vector):
 	return _populate_py(in_snot,in_vector)
+
+def paths(in_snot,base_path="$"):
+	def join_lists(a,b):
+		return a+b
+
+	if hasattr(in_snot,iteritems_attr):
+		return reduce(join_lists,[paths(v,base_path+"["+k+"]") for k,v in getattr(in_snot,iteritems_attr)()])
+	elif isinstance(in_snot,list):
+		return reduce(join_lists,[paths(v,base_path+"["+str(k)+"]") for k,v in _izip(_count(),in_snot)])
+	else:
+		return [str(base_path)]
+
 
 __all__ = ["load","loads","dump","dumps","traverse","populate"]
